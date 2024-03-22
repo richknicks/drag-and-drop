1a389c96eeed4b2596d7a396b7df9f9e7f218c78
Richard Lynch <rlynch2012@gmail.com>
2024-03-22 01:47:14 -0400

    add functionality to update drag and drop iitems  on the DOM and local storage

--- script.js
+++ script.js
@@ -90,14 +90,34 @@ function updateDOM() {
     createItemEl(onHoldList, 0, onHoldItem, index);
   });
   // Run getSavedColumns only once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
 
-
+// Allow arrays to reflect Drag and Drop items
+function rebuildArrays () {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 }
 
 // When Item Starts Dragging
 function drag(e) {
   draggedItem = e.target;
-  console.log('draggedItem:', draggedItem);
 }
 
 // Column Allows for Item to Drop
@@ -121,6 +141,7 @@ function drop (e) {
   // Add Item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 } 
 
 // On Load
